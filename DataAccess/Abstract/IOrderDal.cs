b236fe86df8c59563d8bf63252efedef6eb60ae9
Zeynep <zeynp.inan@3269@gmail.com>
2021-07-06 11:36:29 +0300

    Core katmanı oluşturma , evrensel kod yazıldı

--- DataAccess/Abstract/IOrderDal.cs
+++ DataAccess/Abstract/IOrderDal.cs
@@ -6,8 +6,7 @@ using System.Text;
 
 namespace DataAccess.Abstract
 {
-    public interface IProductDal:IEntityRepository<Product>
+    public interface IOrderDal:IEntityRepository<Order>
     {
-       
     }
 }
