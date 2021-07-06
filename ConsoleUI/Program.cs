403cb5dc7face145df04e5eb621c1c083dd6a3c3
Zeynep <zeynp.inan@3269@gmail.com>
2021-07-06 14:29:05 +0300

    Products ve Categories tabloları join işlemi -- DTO -- GetProductDetails()

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,13 +11,31 @@ namespace ConsoleUI
         //OPEN CLOSED PRINCIPLE: YAPTIĞIN YAZILIMA YENİ BİR ÖZELLİK EKLİYOSAN MEVCUTTAKİ HİÇBİR KODUNA DOKUNAMAZSIN
         static void Main(string[] args)
         {
+            ProductTest();
+
+            //CategoryTest();
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+
+            var c = categoryManager.GetById(3);
+            Console.WriteLine(c.CategoryName);
+
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetAllByCategoryId(2))
+            foreach (var product in productManager.GetProductDetails())
             {
-
-
-                Console.WriteLine(product.ProductName);
+                Console.WriteLine(product.ProductName +" / "+ product.CategoryName);
             }
         }
     }
